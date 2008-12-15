49293214a2cdbcb96a58118c8e4c0a220296a097
Ricardo Chimal, Jr <kiwi@null.cx>
2008-12-15 02:45:58 -0800

    actions: split out migration process into migrate_up and migration_down for individual versions, filled in redo action

--- actions.py
+++ actions.py
@@ -54,28 +54,57 @@ class Migration(object):
 		latest_version = AppliedMigration.latest_version()
 		return sorted([t for t in self.migration_files_with_version() if t[1] > latest_version], key=lambda x: x[1])
 
+	def migration_file(self, version):
+		for t in self.migration_files_with_version():
+			if t[1] == version:
+				return t[0]
+		return None
+
 	def load_migration_model(self, file_path):
 		import imp
 		dir_name, file_name = os.path.split(file_path)
 		mod_name = file_name.replace('.py', '')
 		dot_py_suffix = ('.py', 'U', 1)
 		mod = imp.load_module(mod_name, open(file_path), file_path, dot_py_suffix)
-		return mod#.Migration
+		return mod
 
 	@transaction.commit_manually
 	def run(self):
-		from simplemigrations.models import AppliedMigration
-
 		for t in  self.migrations_to_run():
 			file_name, version = t
 			file_path = os.path.join(settings.MIGRATION_DIRECTORY, file_name)
 			klass = self.load_migration_model(file_path)
-			m = klass.Migration()
-
-			m.run(action='up')
-			AppliedMigration.objects.create(filename=file_name, version=version)
+			self.migrate_up(klass, file_name, version)
 			transaction.commit()
 
+	@transaction.autocommit
+	def migrate_up(self, klass, file_name, version):
+		from simplemigrations.models import AppliedMigration
+
+		m = klass.Migration()
+		m.run(action='up')
+		AppliedMigration.objects.create(filename=file_name, version=version)
+		#transaction.commit_unless_managed()
+
+	@transaction.autocommit
+	def migrate_down(self, klass, instance):
+		m = klass.Migration()
+		m.run(action='down')
+		instance.delete()
+		#transaction.commit_unless_managed()
+
+	@transaction.commit_manually
 	def redo(self):
-		pass
+		from simplemigrations.models import AppliedMigration
+
+		am = AppliedMigration.latest()
+		version = am.version
+		file_name = self.migration_file(version)
+		file_path = os.path.join(settings.MIGRATION_DIRECTORY, file_name)
+		klass = self.load_migration_model(file_path)
+
+		self.migrate_down(klass, am)
+		self.migrate_up(klass, file_name, version)
+
+		transaction.commit()
 
