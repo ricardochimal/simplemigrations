17edb240c269919e43f6f0c042a0d745e1ffab3d
Michael Dippery <mdippery@gmail.com>
2009-05-22 07:58:20 +0800

    Added a setup script.
    Signed-off-by: Ricardo Chimal, Jr. <ricardo@heroku.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="simplemigrations",
+    description="Simple database migration app for Django inspired by Rails",
+    version="1.0",
+    author="Ricardo Chimal, Jr.",
+    url="http://github.com/ricardochimal/simplemigrations/",
+    license="BSD",
+    packages=["simplemigrations"],
+)
